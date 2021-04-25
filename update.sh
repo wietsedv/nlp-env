489d7448c456b5e2fecb6826598cd91fd1a320df
Wietse de Vries <wietsedvries@gmail.com>
2021-04-25 16:16:11 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ set -e
 conda-lock -p osx-arm64 -p linux-64
 
 if ! git diff --exit-code conda-linux-64.lock; then
+    git add .
     git commit -m "update lock [$(date --iso-8601)]"
     git push origin main
 fi
