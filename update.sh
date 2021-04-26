80668d7f4c78ec9282b7f22c93e98183387fc187
Wietse de Vries <wietse.de.vries@rug.nl>
2021-04-26 19:37:17 +0200

    more verbose update script

--- update.sh
+++ update.sh
@@ -3,8 +3,12 @@ set -e
 
 conda-lock -p osx-arm64 -p linux-64
 
-if ! git diff --exit-code conda-linux-64.lock; then
-    git add .
-    git commit -m "update lock [$(date --iso-8601)]"
-    git push origin main
+if git diff --exit-code conda-linux-64.lock; then
+    echo "no changes detected. not pusing new lock file"
+    exit 0
 fi
+
+git add .
+git commit -m "update lock [$(date --iso-8601)]"
+git push origin main
+echo "lock file has been updated"
